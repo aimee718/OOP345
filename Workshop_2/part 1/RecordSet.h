77833fd60c53423876f996874b99799be241f99a
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-04-29 15:32:52 -0400

    Update RecordSet.h

--- Workshop_2/part 1/RecordSet.h
+++ Workshop_2/part 1/RecordSet.h
@@ -1,7 +1,6 @@
 /******************************************
 | Subject        : Workshop 2             |
-| Name           : Eun Kyung (Aimee) Lee  |
-| Student number : 056342132              |
+| Name           : Aimee Lee              |
 | Date           : 10-04-2020             |
 ******************************************/
 // I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
