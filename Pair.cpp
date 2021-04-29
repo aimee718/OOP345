f2cd4e924121fca26317e65ef0713cd0c301a94a
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-04-29 15:34:13 -0400

    Update Pair.cpp

--- Pair.cpp
+++ Pair.cpp
@@ -1,7 +1,6 @@
 /******************************************
 | Subject        : Workshop 3             |
-| Name           : Eun Kyung (Aimee) Lee  |
-| Student number : 056342132              |
+| Name           : Aimee Lee              |
 | Date           : 10-11-2020             |
 ******************************************/
 // I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
