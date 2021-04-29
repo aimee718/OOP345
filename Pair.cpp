9f2fcb968b360b6ac6af227433703332f1ef4f55
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-04-29 15:31:32 -0400

    Add files via upload

---
+++ Pair.cpp
@@ -0,0 +1,19 @@
+/******************************************
+| Subject        : Workshop 3             |
+| Name           : Eun Kyung (Aimee) Lee  |
+| Student number : 056342132              |
+| Date           : 10-11-2020             |
+******************************************/
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Pair.h"
+
+using namespace std;
+using namespace sdds;
+
+namespace sdds {
+
+}
