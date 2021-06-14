02d2b308c40ac6256d8282078ecaa5513b3d9f15
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-06-14 14:59:29 -0400

    Delete Collection.cpp

--- Collection.cpp
+++
@@ -1,7 +0,0 @@
-/******************************************
-| Subject        : Workshop 5             |
-| Name           : Eun Kyung (Aimee) Lee  |
-| Student number : 056342132              |
-| Date           : 10-25-2020             |
-******************************************/
-// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
