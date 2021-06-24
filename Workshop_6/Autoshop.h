c26ba9ce44495609e2276766bfce2d449d2c7247
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-06-24 15:39:58 -0400

    Update and rename Autoshop.h to Workshop_6/Autoshop.h

--- Workshop_6/Autoshop.h
+++ Workshop_6/Autoshop.h
@@ -1,7 +1,6 @@
 /******************************************
 | Subject        : Workshop 6             |
 | Name           : Eun Kyung (Aimee) Lee  |
-| Student number : 056342132              |
 | Date           : 11-08-2020             |
 ******************************************/
 // I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
