f69b501306ba961af0225c408e4e47d67d7545c8
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-06-24 16:17:44 -0400

    Update and rename Station.h to Milestone_1/Station.h

--- Milestone_1/Station.h
+++ Milestone_1/Station.h
@@ -1,7 +1,6 @@
 /******************************************
 | Subject        : Milestone 1            |
 | Name           : Eun Kyung (Aimee) Lee  |
-| Student number : 056342132              |
 | Date           : 11-14-2020             |
 ******************************************/
 // I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
